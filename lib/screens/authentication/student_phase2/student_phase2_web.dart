5e08a9b329a2adb5da5f9cbfd42f216e660c5a2b
Ali Raza <aliahmadraza401@gmail.com>
2022-04-21 22:37:55 +0500

    phone view

--- lib/screens/authentication/student_phase2/student_phase2_web.dart
+++ lib/screens/authentication/student_phase2/student_phase2_web.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:heilo/screens/authentication/Popups/auth_popUp.dart';
