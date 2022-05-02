b59a474efae2ed4c932ac69a09aa95f742edf3a0
unknown <masoomraza752@gmail.com>
2022-05-02 10:50:27 +0500

    admin 6th page

--- lib/main.dart
+++ lib/main.dart
@@ -37,7 +37,7 @@ class MyApp extends StatelessWidget {
           },
           // home: const Intro(),
           // home: const AdminSideMenu(),
-          home: StudentSideMenu(),
+          home: AdminSideMenu(),
         );
       },
     );
